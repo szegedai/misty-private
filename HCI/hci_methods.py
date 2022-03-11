04bd96b75b108aabcc1cf33d0c39f6cba7407acb
lvszeged <99201155+lvszeged@users.noreply.github.com>
2022-03-11 08:53:01 +0100

    Update hci_methods.py
    Cleaning up code

--- HCI/hci_methods.py
+++ HCI/hci_methods.py
@@ -1,5 +1,4 @@
 import cv2
-from time import gmtime, strftime
 
 def default_image_classification_algorithm(image):
     # Write your own method here...
@@ -9,10 +8,4 @@ def default_image_classification_algorithm(image):
     print("Hmm... it looks like a nail!")
 
     cv2.imshow('VIDEO', image)
-    key = cv2.waitKey(1)
     
-    # if the spacebar is pressed
-    if key%256 == 32:
-        img_name = "img_{}.png".format(strftime("%Y%m%d%H%M%S"))
-        cv2.imwrite(img_name, image)
-        cv2.imshow("1", image)
