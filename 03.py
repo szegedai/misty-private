5f7a3754d9197b7724dece0fdc1e0f1d1a664675
Csjernyik István <csjeristvan@gmail.com>
2022-03-25 12:14:31 +0100

    ffmpeg video stream

--- 03.py
+++ 03.py
@@ -8,6 +8,7 @@ from time import gmtime, strftime
 # https://stackoverflow.com/questions/70813361/how-to-extract-video-and-audio-from-ffmpeg-stream-in-python
 # https://github.com/kkroening/ffmpeg-python/blob/master/examples/tensorflow_stream.py
 
+# "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4" -teszt stream
 stream_uri = "rtsp://10.2.8.5:1936"
 width = 640
 height = 480
@@ -15,8 +16,17 @@ height = 480
 def start_ffmpeg_video_process(input_uri):
     args = (
         ffmpeg
-        .input(input_uri, format='rawvideo', pix_fmt='rgb24')
-        .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+        .input(input_uri, r = 30) # r-nek bármit megadva működik
+        .output('pipe:', format='rawvideo', pix_fmt='bgr24')
+        .compile()
+    )
+    return subprocess.Popen(args, stdout=subprocess.PIPE)
+
+def start_ffmpeg_audio_process(input_uri):
+    args = (
+        ffmpeg
+        .input(stream_uri, format='s16le')
+        .output('pipe:', format='s16le')
         .compile()
     )
     return subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -35,16 +45,25 @@ def read_video_frame(process, width, height):
         )
     return frame
 
+def read_audio(process):
+    return process.stdout.read()
+
 def process_frame(frame):
-    print(frame)
+    #print(frame)
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    cv2.imshow('video', frame)
+    cv2.waitKey(1)
 
 process = start_ffmpeg_video_process(stream_uri)
+process2 = start_ffmpeg_audio_process(stream_uri)
+
 
 while True:
-    print("Reading frame.")
+    #print("Reading frame.")
     in_frame = read_video_frame(process, width, height)
     if in_frame is None:
         break
-    
+
     process_frame(in_frame)
 
+    print(read_audio(process2))
